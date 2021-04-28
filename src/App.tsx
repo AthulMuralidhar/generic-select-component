8c14fcf5515cf7f2ee012fe636d3a87ae33842de
Athul Muralidhar <athul.m68@gmail.com>
2021-04-28 10:06:33 +0200

    added custom select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Console } from 'node:console';
+
+// types
+type Option = {
+  value: string
+  
+}
+
+
+// logic
+const Select = (props) => {
+  const handleOnChange(e){
+    props.onChange(e.currentSelect.value)
+  }
+
+  return (
+    <select value={props.value} onChange={handleOnChange}>
+      {props.options.map (option =>(
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      )}
+    </select>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const selects = [
+    { value: "es3", label: "ECMAScript 3" },
+    { value: "es5", label: "ECMAScript 5" },
+    { value: "es2015", label: "ECMAScript 2015" },
+    { value: "es2016", label: "ECMAScript 2016" },
+    { value: "es2017", label: "ECMAScript 2017" },
+    { value: "es2018", label: "ECMAScript 2018" },
+    { value: "es2019", label: "ECMAScript 2019" }
+  ]
+
+  const [select, setSelect] = useState("es2019")
+  return <select></select>
 }
 
 export default App;
